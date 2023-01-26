1f95007cb9fa79f016b2e985a847c725df4b3efd
Alex <agonzalez.alebrije@gmail.com>
2023-01-26 00:17:56 -0600

    Add Interactive Pricing Component

--- interactive-pricing-component/src/App.tsx
+++ interactive-pricing-component/src/App.tsx
@@ -1,9 +1,21 @@
 import "./styles.scss";
-import { useRef } from "react";
+import { useRef, useState } from "react";
+import Checkmark from "./media/icon-check.svg";
 
 function App() {
+  //Range slider
   const sliderRef = useRef(null);
-  console.log(sliderRef);
+  const [sliderValue, setSliderValue] = useState(16);
+  const viewsPerDollar = sliderValue * 6.25;
+
+  //Toggle switch
+  const [pricing, setPricing] = useState(false);
+  const monthlyCost = sliderValue;
+  const annualCost = sliderValue * 12 * 0.75;
+
+  function togglePricing() {
+    setPricing((prevMode) => !prevMode);
+  }
 
   return (
     <div className="App">
@@ -17,26 +29,62 @@ function App() {
       </header>
       <main>
         <div></div>
-        <p className="page-views">100K Pageviews</p>
+        <p className="page-views">{`${viewsPerDollar}K Pageviews`}</p>
         <div className="amount-per-period">
-          <span className="amount">16</span>
+          <span className="amount">{`$${
+            !pricing ? monthlyCost : annualCost
+          }`}</span>
           <span className="period">/ month</span>
         </div>
         <div className="slide-container">
           <input
+            onChange={() => {
+              if (sliderRef.current != null) {
+                setSliderValue(sliderRef.current["value"]);
+              }
+            }}
             type="range"
-            min="1"
+            className="range-slider"
+            value={sliderValue}
+            min="2"
             max="32"
-            defaultValue="16"
-            className="slider"
-            id="myRange"
+            step="2"
             ref={sliderRef}
           />
         </div>
-        <p>
-          Monthly Billing Yearly Billing 25% discount Unlimited websites 100%
-          data ownership Email reports Start my trial
-        </p>
+        <div className="billing-grouping">
+          <div className="toggle-info">
+            <p className="toggle-details">Monthly Billing</p>
+            <label className="switch">
+              <input
+                type="checkbox"
+                onClick={togglePricing}
+                aria-label="toggle-pricing"
+              />
+              <div className="round slider"></div>
+            </label>
+            <p className="toggle-details">Yearly Billing</p>
+          </div>
+          <p className="discount">25% discount</p>
+        </div>
+        <hr />
+        <div className="benefits-grouping">
+          <div className="benefits">
+            <div className="checkmark-grouping">
+              <img src={Checkmark} alt="checkmark" className="checkmark" />
+              <p>Unlimited websites</p>
+            </div>
+            <div className="checkmark-grouping">
+              <img src={Checkmark} alt="checkmark" className="checkmark" />
+              <p>100% data ownership</p>
+            </div>
+            <div className="checkmark-grouping">
+              <img src={Checkmark} alt="checkmark" className="checkmark" />
+              <p>Email reports</p>
+            </div>
+          </div>
+          <button className="trial-btn">Start my trial</button>
+        </div>
       </main>
       <footer className="attribution">
         Challenge by{" "}
