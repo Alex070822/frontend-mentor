2f3cb4963c194f1f745e776b403a7294e67d9273
Alex <agonzalez.alebrije@gmail.com>
2023-02-08 02:01:05 -0600

    Add decimals to amount and fixed range slider background

--- interactive-pricing-component/src/components/Main.tsx
+++ interactive-pricing-component/src/components/Main.tsx
@@ -16,12 +16,30 @@ function Main() {
   function togglePricing() {
     setPricing((prevMode) => !prevMode);
   }
+
+  if (sliderRef.current != null) {
+    const input = sliderRef.current as HTMLInputElement;
+
+    const value = Number(input.value);
+    const min = Number(input.min);
+    const max = Number(input.max);
+
+    let progress = ((value - min) / (max - min)) * 100;
+
+    input.style.background =
+      "linear-gradient(to right, #A4F3EB 0%, #A4F3EB " +
+      progress +
+      "%, #ECF0FB " +
+      value +
+      "%, #ECF0FB 100%)";
+  }
+
   return (
     <main>
       <p className="page-views">{`${viewsPerDollar}K Pageviews`}</p>
       <div className="amount-per-period">
         <span className="amount">{`$${
-          !pricing ? monthlyCost : annualCost
+          !pricing ? monthlyCost.toFixed(2) : annualCost.toFixed(2)
         }`}</span>
         <span className="period">/ month</span>
       </div>
@@ -29,7 +47,8 @@ function Main() {
         <input
           onChange={() => {
             if (sliderRef.current != null) {
-              setSliderValue(sliderRef.current["value"]);
+              const value = Number(sliderRef.current["value"]);
+              setSliderValue(value);
             }
           }}
           type="range"
