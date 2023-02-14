de5fc947083ae4f9d6b0f0ba751d6cfa82ca8877
Alex <agonzalez.alebrije@gmail.com>
2023-02-14 16:56:38 -0600

    Add Card and UserDashboard components

---
+++ time-tracking-dashboard/src/components/UserDashboard.tsx
@@ -0,0 +1,12 @@
+function UserDashboard() {
+  return (
+    <div className="dashboard-main-container">
+      <h2 className="dashboard-secondary-container">
+        Report for Jeremy Robson
+      </h2>
+      Daily Weekly Monthly
+    </div>
+  );
+}
+
+export default UserDashboard;
