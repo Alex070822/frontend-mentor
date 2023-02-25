e05f38b5b43081c466ddcd4477eb6bac9e72afa4
Alex GH <110792798+alex070822@users.noreply.github.com>
2023-02-24 19:51:12 -0600

    Add Main.tsx

--- planets-fact-site/src/App.tsx
+++ planets-fact-site/src/App.tsx
@@ -1,8 +1,15 @@
 import React from "react";
 import "./styles.scss";
+import Header from "./components/Header";
+import Main from "./components/Main";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+      <Main />
+    </div>
+  );
 }
 
 export default App;
