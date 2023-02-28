1e7ccc4f0a96d00a4bddd1a90713e1459455d14d
Alex <agonzalez.alebrije@gmail.com>
2023-02-28 13:29:00 -0600

    Update Navbar menu

--- planets-fact-site/src/components/Navbar.tsx
+++ planets-fact-site/src/components/Navbar.tsx
@@ -8,7 +8,6 @@ function Navbar() {
       <input type="checkbox" className="nav-checkbox" id="nav-checkbox" />
       <label htmlFor="nav-checkbox" className="nav-toggle">
         <img src={navbarIcon} alt="Navbar icon" className="nav-toggle-menu" />
-        <img src={navbarIcon} alt="Navbar icon" className="nav-toggle-close" />
       </label>
       <ul className="nav-menu">
         <li>
