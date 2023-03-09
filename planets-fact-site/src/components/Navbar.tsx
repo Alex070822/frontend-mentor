fee08a48203646b112257a3c373648a15562d545
Alex GH <110792798+alex070822@users.noreply.github.com>
2023-03-09 00:16:55 -0600

    Add Navbar toggle

--- planets-fact-site/src/components/Navbar.tsx
+++ planets-fact-site/src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Navbar({
   planetsNames,
@@ -7,20 +7,35 @@ function Navbar({
   planetsNames: string[];
   setSelectedPlanet: (name: string) => void;
 }) {
+  const [navbarToggle, setNavbarToggle] = useState(false);
+
   const navbarIcon = require("../media/icon-hamburger.svg").default;
   const listIcon = require("../media/icon-chevron.svg").default;
 
+  function toggleNavbar() {
+    setNavbarToggle((prevMode) => !prevMode);
+  }
+
   return (
     <nav className="nav">
-      <input type="checkbox" className="nav-checkbox" id="nav-checkbox" />
-      <label htmlFor="nav-checkbox" className="nav-toggle">
-        <img src={navbarIcon} alt="Navbar icon" className="nav-toggle-menu" />
-      </label>
-      <ul className="nav-menu">
+      <img
+        src={navbarIcon}
+        alt="Navbar icon"
+        className={!navbarToggle ? "nav-toggle-menu" : "nav-toggle-menu-close"}
+        onClick={toggleNavbar}
+      />
+      <ul className={navbarToggle ? "nav-menu" : "nav-menu-hidden"}>
         {planetsNames.map((name) => (
           <li key={name}>
             <div className="nav-menu-circle"></div>
-            <a onClick={() => setSelectedPlanet(name)}>{name}</a>
+            <a
+              onClick={() => {
+                setSelectedPlanet(name);
+                toggleNavbar();
+              }}
+            >
+              {name}
+            </a>
             <img src={listIcon} alt="list icon" className="list-icon" />
           </li>
         ))}
