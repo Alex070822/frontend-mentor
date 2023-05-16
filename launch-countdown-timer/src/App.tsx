c35c7bab57c362a86d3f0f985ceeffbb0f19bc43
Alex GH <110792798+alex070822@users.noreply.github.com>
2023-05-15 22:29:48 -0600

    Add timer logic

--- launch-countdown-timer/src/App.tsx
+++ launch-countdown-timer/src/App.tsx
@@ -1,13 +1,55 @@
 import "./App.css";
 import Countdown from "./components/Countdown/Countdown";
 import Footer from "./components/Footer/Footer";
+import { useEffect, useState } from "react";
+
+export interface TimeLeft {
+  days: number;
+  hours: number;
+  minutes: number;
+  seconds: number;
+}
 
 function App() {
+  const [timeLeft, setTimeLeft] = useState<TimeLeft>({
+    days: 0,
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+  });
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setTimeLeft(calculateTimeLeft());
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  });
+
+  function calculateTimeLeft(): TimeLeft {
+    const targetDate = new Date();
+    targetDate.setDate(targetDate.getDate() + 8);
+    targetDate.setHours(23, 55, 41, 0);
+    const difference = +targetDate - +new Date();
+    let timeLeft: TimeLeft = { days: 0, hours: 0, minutes: 0, seconds: 0 };
+
+    if (difference > 0) {
+      timeLeft = {
+        days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+        hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+        minutes: Math.floor((difference / 1000 / 60) % 60),
+        seconds: Math.floor((difference / 1000) % 60),
+      };
+    }
+
+    return timeLeft;
+  }
+
   return (
     <div className="App">
       <main>
         <h1>We're launching soon</h1>
-        <Countdown />
+        <Countdown timeLeft={timeLeft} />
       </main>
       <Footer />
     </div>
--- launch-countdown-timer/src/components/Countdown/Countdown.tsx
+++ launch-countdown-timer/src/components/Countdown/Countdown.tsx
@@ -1,6 +1,14 @@
 import "./countdown.css";
+import { FC } from "react";
+import { TimeLeft } from "../../App";
 
-function Countdown() {
+interface CountdownProps {
+  timeLeft: TimeLeft;
+}
+
+const Countdown: FC<CountdownProps> = ({
+  timeLeft: { days, hours, minutes, seconds },
+}: CountdownProps) => {
   return (
     <div className="counter-container">
       <div className="counter">
@@ -9,9 +17,9 @@ function Countdown() {
             <div className="left-card-hole"></div>
             <div className="right-card-hole"></div>
             <div className="top-transparency"></div>
-            <div className="card-top">55</div>
+            <div className="card-top">{days}</div>
             <div className="divider"></div>
-            <div className="card-bottom">55</div>
+            <div className="card-bottom">{days}</div>
           </div>
         </div>
         <div className="time">DAYS</div>
@@ -22,9 +30,9 @@ function Countdown() {
             <div className="left-card-hole"></div>
             <div className="right-card-hole"></div>
             <div className="top-transparency"></div>
-            <div className="card-top">55</div>
+            <div className="card-top">{hours}</div>
             <div className="divider"></div>
-            <div className="card-bottom">55</div>
+            <div className="card-bottom">{hours}</div>
           </div>
         </div>
         <div className="time">HOURS</div>
@@ -35,9 +43,9 @@ function Countdown() {
             <div className="left-card-hole"></div>
             <div className="right-card-hole"></div>
             <div className="top-transparency"></div>
-            <div className="card-top">55</div>
+            <div className="card-top">{minutes}</div>
             <div className="divider"></div>
-            <div className="card-bottom">55</div>
+            <div className="card-bottom">{minutes}</div>
           </div>
         </div>
         <div className="time">MINUTES</div>
@@ -48,15 +56,15 @@ function Countdown() {
             <div className="left-card-hole"></div>
             <div className="right-card-hole"></div>
             <div className="top-transparency"></div>
-            <div className="card-top">55</div>
+            <div className="card-top">{seconds}</div>
             <div className="divider"></div>
-            <div className="card-bottom">55</div>
+            <div className="card-bottom">{seconds}</div>
           </div>
         </div>
         <div className="time">SECONDS</div>
       </div>
     </div>
   );
-}
+};
 
 export default Countdown;
